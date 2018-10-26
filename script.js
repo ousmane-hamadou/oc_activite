f9a49d6c8f843e7e0a6528463a7e605191bca6dc
ousmane-hamadou <ousmanehamadou776@gmail.com>
2018-10-27 01:46:31 +0200

    ajout d'une nouvelle script qui affiche une message dans la console avant d'afficher le contenu du champ de saisie

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var btn = document.getElementById('btn');
 btn.addEventListener('click',function(){
+    console.log('Vous avez soumis le formulaire')
     var name = document.querySelector('[placeholder]').value
     alert(name)
 })
